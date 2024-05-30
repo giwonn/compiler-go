ee46f14c588c6df65d9c06e8848e7f5190271573
Allen <allendev@alphacode.co.kr>
2024-05-30 14:34:06 +0900

    bool 연산 컴파일 구현

--- code/code.go
+++ code/code.go
@@ -14,6 +14,8 @@ const (
 	OpSub
 	OpMul
 	OpDiv
+	OpTrue
+	OpFalse
 )
 
 type Opcode byte
@@ -30,6 +32,8 @@ var definitions = map[Opcode]*Definition{
 	OpSub:      {"OpSub", []int{}},
 	OpMul:      {"OpMul", []int{}},
 	OpDiv:      {"OpDiv", []int{}},
+	OpTrue:     {"OpTrue", []int{}},
+	OpFalse:    {"OpFalse", []int{}},
 }
 
 // Lookup 존재하는 opcode인지 체크
