ddba65965e7578d89d300410ff28e32d993a150e
Allen <allendev@alphacode.co.kr>
2024-05-30 18:44:34 +0900

    전위 표현식 컴파일 구현

--- code/code.go
+++ code/code.go
@@ -19,6 +19,8 @@ const (
 	OpEqual
 	OpNotEqual
 	OpGreaterThan
+	OpMinus
+	OpBang
 )
 
 type Opcode byte
@@ -40,6 +42,8 @@ var definitions = map[Opcode]*Definition{
 	OpEqual:       {"OpEqual", []int{}},
 	OpNotEqual:    {"OpNotEqual", []int{}},
 	OpGreaterThan: {"OpGreaterThan", []int{}},
+	OpMinus:       {"OpMinus", []int{}},
+	OpBang:        {"OpBang", []int{}},
 }
 
 // Lookup 존재하는 opcode인지 체크
